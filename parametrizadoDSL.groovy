47aef8f4b9dc9e78fae5e284b6fcf000944a08f0
Jonatan Stiven Gutierrez Nieto <107326932+jonatangg10@users.noreply.github.com>
2024-02-28 20:56:46 -0500

    Update parametrizadoDSL.groovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,4 +1,4 @@
-job('ejemplo-job-DSL') {
+job('ejemplo2-job-DSL') {
   	description('Job DSL de ejemplo para el curso de Jenkins')
   	scm {
     	git('https://github.com/jonatangg10/pipeline-variables.git', 'main') { node ->
